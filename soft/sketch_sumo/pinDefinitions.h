17c0cbb7a3b666798743732e050ee1fc0abef9d2
blamster19 <blamster19@gmail.com>
2023-06-10 10:16:43 +0200

    fix: redefine pins to avoid collisions
    Also put line sensors into interrupt pins for future experiments.

--- soft/sketch_sumo/pinDefinitions.h
+++ soft/sketch_sumo/pinDefinitions.h
@@ -1,23 +1,23 @@
 #ifndef DEFINITIONS_H
 #define DEFINITIONS_H
 
-#define SONIC_TRIGGER1 1
-#define SONIC_TRIGGER2 2
-#define SONIC_TRIGGER3 3
-#define SONIC_ECHO1    4
-#define SONIC_ECHO2    5
-#define SONIC_ECHO3    6
-#define IR_PIN         7
-#define LINE_PIN1      8
-#define LINE_PIN2      9
-#define LINE_PIN3      10
-#define LINE_PIN4      11
-#define LINE_PIN5      12
-#define LINE_PIN6      13
-#define SHOCK_PIN      14
-#define MOTOR_PWM_1    3
+#define SONIC_TRIGGER1 8
+#define SONIC_TRIGGER2 10
+#define SONIC_TRIGGER3 12
+#define SONIC_ECHO1    9
+#define SONIC_ECHO2    11
+#define SONIC_ECHO3    13
+#define IR_PIN         14
+#define LINE_PIN1      0
+#define LINE_PIN2      1
+#define LINE_PIN3      2
+#define LINE_PIN4      3
+#define LINE_PIN5      15
+#define LINE_PIN6      16
+#define SHOCK_PIN      17
+#define MOTOR_PWM_1    5
 #define MOTOR_DIR_1    4
-#define MOTOR_PWM_2    9
+#define MOTOR_PWM_2    6
 #define MOTOR_DIR_2    7
 
 #define MAX_SONIC      50
