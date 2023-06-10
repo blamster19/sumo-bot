ea1756022c184f7e363bca81bb60450e97215534
blamster19 <blamster19@gmail.com>
2023-06-10 18:03:49 +0200

    feat: add line avoidance behavior
    This behavior is controlled via interrupts; robot will either turn left, right or bo back and turn around.

--- soft/sketch_sumo/behavior.ino
+++ soft/sketch_sumo/behavior.ino
@@ -2,10 +2,30 @@ void mainLoop() {
 	
 }
 
-void avoidFrontLeft() {
+#define maxSpeed 255
 
+void avoidFront() {
+	goForward(-maxSpeed); // go back
+	delay(1500);
+	goTurnR(maxSpeed); // turn around
+	delay(350);
+}
+
+void avoidFrontLeft() {
+	if(readLine(LINE_PIN2)) {
+		goTurnR(maxSpeed); // turn right
+		delay(350);
+	} else {
+		avoidFront(); // both sensors see the line
+	}
 }
 
 void avoidFrontRight() {
-
+	if(readLine(LINE_PIN1)) {
+		goTurnL(maxSpeed); // turn left
+		delay(350);
+	} else {
+		avoidFront(); // both sensors see the line
+	}
 }
+
