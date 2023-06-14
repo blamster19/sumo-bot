aff257545a482ea63f1faa6f304ddc1dd46ea930
blamster19 <blamster19@gmail.com>
2023-06-14 08:24:44 +0200

    refactor: remove pin setup, use new motor control
    Removed old sensor pin states and used goForward() to set the speed to 0 instead of direc motor control from previous library.

--- soft/sketch_sumo/sketch_sumo.ino
+++ soft/sketch_sumo/sketch_sumo.ino
@@ -1,6 +1,6 @@
 #include <NewPing.h>
 #include <IRremote.h>
-#include <CytronMotorDriver.h>
+#include <Servo.h>
 #include "pinDefinitions.h"
 
 NewPing sonar1(SONIC_TRIGGER1, SONIC_ECHO1, MAX_SONIC);
@@ -8,19 +8,11 @@ NewPing sonar2(SONIC_TRIGGER2, SONIC_ECHO2, MAX_SONIC);
 NewPing sonar3(SONIC_TRIGGER3, SONIC_ECHO3, MAX_SONIC);
 IRrecv  irrecv(IR_PIN);
 decode_results ir1;
-CytronMD motorL(PWM_DIR, MOTOR_PWM_1, MOTOR_DIR_1);
-CytronMD motorR(PWM_DIR, MOTOR_PWM_2, MOTOR_DIR_2);
-
 
 void setup() {
 	irrecv.enableIRIn();
 	pinMode(LINE_PIN1, INPUT_PULLUP);
 	pinMode(LINE_PIN2, INPUT_PULLUP);
-	pinMode(LINE_PIN3, INPUT);
-	pinMode(LINE_PIN4, INPUT);
-	pinMode(LINE_PIN5, INPUT);
-	pinMode(LINE_PIN6, INPUT);
-	pinMode(SHOCK_PIN, INPUT);
 	attachInterrupt(digitalPinToInterrupt(LINE_PIN1), avoidFrontLeft, LOW);
 	attachInterrupt(digitalPinToInterrupt(LINE_PIN2), avoidFrontRight, LOW);
 }
@@ -47,8 +39,7 @@ void loop() {
 	if(onOff) {
 		mainLoop();
 	} else {
-		motorR.setSpeed(0);
-		motorL.setSpeed(0);
+		goForward(0);
 	}
 	delay(75);
 }
