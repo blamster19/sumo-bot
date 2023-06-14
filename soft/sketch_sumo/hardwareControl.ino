60885f2fe0b5fc9f70b67974636177a7629cd41a
blamster19 <blamster19@gmail.com>
2023-06-14 08:23:40 +0200

    refactor: rewrite motor control
    Rewritten motor control code using `Servo.h` library.

--- soft/sketch_sumo/hardwareControl.ino
+++ soft/sketch_sumo/hardwareControl.ino
@@ -3,41 +3,37 @@ bool readLine(byte pin) {
 	return digitalRead(pin) == HIGH;
 }
 
-bool readShock() {
-	return digitalRead(SHOCK_PIN) == HIGH;
-}
-
 int readSonic(NewPing sonar) {
 	return sonar.ping_cm() + 1;// sensor gives distance 1 cm shorter than real value
 }
 
 // MOTORS
 void goForward(byte speed) {
-	motorL.setSpeed(speed);
-	motorR.setSpeed(speed);
+	motor(1, FORWARD, speed);
+	motor(2, BACKWARD, speed);
 }
 
 void goBackwards(byte speed) {
-	motorL.setSpeed(-speed);
-	motorR.setSpeed(-speed);
+	motor(1, BACKWARD, speed);
+	motor(2, FORWARD, speed);
 }
 
 void goTurnR(byte speed) {
-	motorL.setSpeed(speed);
-	motorR.setSpeed(-speed);
+	motor(1, BACKWARD, speed);
+	motor(2, FORWARD, speed);
 }
 
 void goArcR(byte speed, byte delta) {
-	motorL.setSpeed(speed + delta);
-	motorR.setSpeed(speed - delta);
+	motor(1, BACKWARD, speed + delta);
+	motor(2, FORWARD, speed - delta);
 }
 
 void goTurnL(byte speed) {
-	motorL.setSpeed(-speed);
-	motorR.setSpeed(speed);
+	motor(1, FORWARD, speed);
+	motor(2, BACKWARD, speed);
 }
 
 void goArcL(byte speed, byte delta) {
-	motorL.setSpeed(speed - delta);
-	motorR.setSpeed(speed + delta);
+	motor(1, FORWARD, speed - delta);
+	motor(2, BACKWARD, speed + delta);
 }
---
+++ soft/sketch_sumo/servo.ino
@@ -0,0 +1,109 @@
+//utilize Servo.h
+void motor(int nMotor, int command, int speed)
+{
+  int motorA, motorB;
+
+  if (nMotor >= 1 && nMotor <= 4)
+  {  
+    switch (nMotor)
+    {
+    case 1:
+      motorA   = MOTOR_A_1;
+      motorB   = MOTOR_B_1;
+      break;
+    case 3:
+      motorA   = MOTOR_A_2;
+      motorB   = MOTOR_B_2;
+      break;
+    default:
+      break;
+    }
+
+    switch (command)
+    {
+    case FORWARD:
+      motor_output (motorA, HIGH, speed);
+      motor_output (motorB, LOW, -1);     // -1: no PWM set
+      break;
+    case BACKWARD:
+      motor_output (motorA, LOW, speed);
+      motor_output (motorB, HIGH, -1);    // -1: no PWM set
+      break;
+    case BRAKE:
+      motor_output (motorA, LOW, 255); // 255: fully on.
+      motor_output (motorB, LOW, -1);  // -1: no PWM set
+      break;
+    case RELEASE:
+      motor_output (motorA, LOW, 0);  // 0: output floating.
+      motor_output (motorB, LOW, -1); // -1: no PWM set
+      break;
+    default:
+      break;
+    }
+  }
+}
+
+void motor_output (int output, int high_low, int speed)
+{
+  int motorPWM;
+
+  switch (output)
+  {
+  case MOTOR_A_1:
+  case MOTOR_B_1:
+    motorPWM = MOTOR_PWM_1;
+    break;
+  case MOTOR_A_2:
+  case MOTOR_B_2:
+    motorPWM = MOTOR_PWM_2;
+    break;
+  default:
+    // Use speed as error flag, -3333 = invalid output.
+    speed = -3333;
+    break;
+  }
+
+  if (speed != -3333)
+  {
+    shiftWrite(output, high_low);
+
+    // set PWM only if it is valid
+    if (speed >= 0 && speed <= 255)    
+    {
+      analogWrite(motorPWM, speed);
+    }
+  }
+}
+
+void shiftWrite(int output, int high_low)
+{
+  static int latch_copy;
+  static int shift_register_initialized = false;
+
+  if (!shift_register_initialized)
+  {
+    pinMode(MOTORLATCH, OUTPUT);
+    pinMode(MOTORENABLE, OUTPUT);
+    pinMode(MOTORDATA, OUTPUT);
+    pinMode(MOTORCLK, OUTPUT);
+
+    digitalWrite(MOTORDATA, LOW);
+    digitalWrite(MOTORLATCH, LOW);
+    digitalWrite(MOTORCLK, LOW);
+    digitalWrite(MOTORENABLE, LOW);
+
+    latch_copy = 0;
+
+    shift_register_initialized = true;
+  }
+
+  bitWrite(latch_copy, output, high_low);
+
+  shiftOut(MOTORDATA, MOTORCLK, MSBFIRST, latch_copy);
+  delayMicroseconds(5);
+  digitalWrite(MOTORLATCH, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(MOTORLATCH, LOW);
+}
+
+
