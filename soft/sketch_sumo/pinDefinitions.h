8e9e054c24ed176788078085cf35212072fe8d9a
blamster19 <blamster19@gmail.com>
2023-06-14 12:48:52 +0200

    refactor: redefine pins

--- soft/sketch_sumo/pinDefinitions.h
+++ soft/sketch_sumo/pinDefinitions.h
@@ -1,13 +1,13 @@
 #ifndef DEFINITIONS_H
 #define DEFINITIONS_H
 
-#define SONIC_TRIGGER1 8
-#define SONIC_TRIGGER2 10
-#define SONIC_TRIGGER3 12
-#define SONIC_ECHO1    9
-#define SONIC_ECHO2    11
-#define SONIC_ECHO3    13
-#define IR_PIN         A0
+#define SONIC_TRIGGER1 A0
+#define SONIC_TRIGGER2 A2
+#define SONIC_TRIGGER3 A4
+#define SONIC_ECHO1    A1
+#define SONIC_ECHO2    A3
+#define SONIC_ECHO3    A5
+#define IR_PIN         13
 #define LINE_PIN1      2
 #define LINE_PIN2      3
 #define MOTOR_A_1      2
