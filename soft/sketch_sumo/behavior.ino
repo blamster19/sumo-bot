30ac902598dfa5dbe3811766628a5f43cd88eee7
blamster19 <blamster19@gmail.com>
2023-06-14 12:52:27 +0200

    feat: line sensors wont work when robot deactivated
    Now the interrupts won't trigger unless `onOff` is true.

--- soft/sketch_sumo/behavior.ino
+++ soft/sketch_sumo/behavior.ino
@@ -34,27 +34,33 @@ void mainLoop() {
 }
 
 void avoidFront() {
-	goBackwards(-maxSpeed); // go back
-	delay(1500);
-	goTurnR(maxSpeed); // turn around
-	delay(350);
+	if(onOff) {
+		goBackwards(maxSpeed); // go back
+		delay(1500);
+		goTurnR(maxSpeed); // turn around
+		delay(350);
+	}
 }
 
 void avoidFrontLeft() {
-	if(readLine(LINE_PIN2)) {
-		goTurnR(maxSpeed); // turn right
-		delay(350);
-	} else {
-		avoidFront(); // both sensors see the line
+	if(onOff) {
+		if(readLine(LINE_PIN2)) {
+			goTurnR(maxSpeed); // turn right
+			delay(350);
+		} else {
+			avoidFront(); // both sensors see the line
+		}
 	}
 }
 
 void avoidFrontRight() {
-	if(readLine(LINE_PIN1)) {
-		goTurnL(maxSpeed); // turn left
-		delay(350);
-	} else {
-		avoidFront(); // both sensors see the line
+	if(onOff) {
+		if(readLine(LINE_PIN1)) {
+			goTurnL(maxSpeed); // turn left
+			delay(350);
+		} else {
+			avoidFront(); // both sensors see the line
+		}
 	}
 }
 
