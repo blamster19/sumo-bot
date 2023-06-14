aa027d14398592eb00bda317d879fa08d99216df
blamster19 <blamster19@gmail.com>
2023-06-14 08:21:30 +0200

    refactor: ground for new motor library
    Due to fried previous shield, a new one will be used.

--- soft/sketch_sumo/pinDefinitions.h
+++ soft/sketch_sumo/pinDefinitions.h
@@ -10,11 +10,22 @@
 #define IR_PIN         A0
 #define LINE_PIN1      2
 #define LINE_PIN2      3
-#define MOTOR_PWM_1    5
-#define MOTOR_DIR_1    4
+#define MOTOR_A_1      2
+#define MOTOR_B_1      3
+#define MOTOR_A_2      5
+#define MOTOR_B_2      7
+#define MOTOR_PWM_1    11
 #define MOTOR_PWM_2    6
-#define MOTOR_DIR_2    7
+#define MOTORLATCH 12
+#define MOTORCLK 4
+#define MOTORENABLE 7
+#define MOTORDATA 8
 
 #define MAX_SONIC      50
+
+#define FORWARD 1
+#define BACKWARD 2
+#define BRAKE 3
+#define RELEASE 4
 
 #endif
